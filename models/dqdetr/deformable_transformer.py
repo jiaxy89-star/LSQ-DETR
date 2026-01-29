13e9d468cb217f6f1167eb6efb6c0bb8b9bcee3b
jiaxy89-star <jiaxy89@gmail.com>
2026-01-29 13:53:36 +0800

    Update deformable_transformer.py

--- models/dqdetr/deformable_transformer.py
+++ models/dqdetr/deformable_transformer.py
@@ -1,4 +1,8 @@
 # ------------------------------------------------------------------------
+# DQ-DETR
+# Copyright (c) 2022 IDEA. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
 # DINO
 # Copyright (c) 2022 IDEA. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 [see LICENSE for details]
